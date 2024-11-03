d16c18a39a90e8282a3dc94cbc2899ad6b51f44e
Angel Jame <50709971+AngelJame@users.noreply.github.com>
2024-11-03 17:12:46 +0800

    Update Uncertainty.R

--- Code/Market uncertainty calculation code/Stochastic Volatility model/Uncertainty.R
+++ Code/Market uncertainty calculation code/Stochastic Volatility model/Uncertainty.R
@@ -1,15 +1,8 @@
 library(stochvol)
 library(tseries)
-datas <- read.csv("请输入文件路径\\idd_all.csv",header=F)
+datas <- read.csv("Enter the location of day_idd.csv",header=F)
 volatility<-c()
 for(i in 1:ncol(datas)){
-  
-  #vdatas<-datas[,i]
-  #print(adf.test(vdatas))
-  
-  #第一步，准备数据，这里原文是将价格数据demean处理，而我们这里检验平稳性即可
-  #该方法是核心方法，本文将先验参数全部设置为默认
-  #             取10000个数据，舍弃前1000个，超参数设置默认数值·
   vdatas<-svsample(as.numeric(datas[,i],draws=10000,burnin=5000))
   volatility<-append(volatility,vdatas$summary$sd[,4])
 }
